0e5ac3e4c7296ae400489abbc8b4b8e7e7f7de2d
yzy <499058564@qq.ocm>
2026-07-09 23:32:32 +0800

    fate(common,app-ui): 增加日志配置

--- app-ui/build.gradle.kts
+++ app-ui/build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
     // JavaFX 主题库
     implementation("io.github.mkpaz:atlantafx-base:2.0.1")
 
+    // SLF4J API
+    implementation("org.slf4j:slf4j-api:2.0.9")
+
     // 日志实现
     runtimeOnly("ch.qos.logback:logback-classic:1.4.14")
 }
